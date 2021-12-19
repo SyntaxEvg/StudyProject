1514d401dd641e5fdcbf46f92aa6222f0781d911
SyntaxeEvgen <jjw2013@ya.ru>
2021-12-19 11:17:55 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StudyProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("fewfewewfwe");
+        }
+    }
+}

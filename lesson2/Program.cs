53779bdbaa6f7099315a25d9475998eeb3566492
SyntaxeEvgen <jjw2013@ya.ru>
2021-12-19 11:19:18 +0300

    2 проекта

---
+++ lesson2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lesson2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("H222!");
+        }
+    }
+}
